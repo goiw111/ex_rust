144107eb65be39363b1fa8331e79cf02dcbe1c73
sohayl chahmot <chahmot.s@gmail.com>
2021-01-17 20:09:00 +0100

    convtopc pascal-tra

---
+++ pascals-triangle/src/lib.rs
@@ -0,0 +1,13 @@
+pub struct PascalsTriangle(u32);
+
+impl PascalsTriangle {
+    pub fn new(row_count: u32) -> Self {
+        PascalsTriangle(row_count)
+    }
+
+    pub fn rows(&self) -> Vec<Vec<u32>> {
+        for i in 0..=self.0 {
+            
+        }
+    }
+}
