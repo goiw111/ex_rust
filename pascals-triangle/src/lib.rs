108dc892aa44eec146c69f11b70dcc3e4188f982
larahack <sohayl@localhost.localdomain>
2021-01-19 04:36:45 +0100

    pascals-triangle

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -6,8 +6,18 @@ impl PascalsTriangle {
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        for i in 0..=self.0 {
-            
+        let mut v = Vec::new();
+        for l in 0..self.0 {
+            let mut c = Vec::new();
+            for i in 0..=l {
+                if i == 0 || l == 0 {
+                    c.push(1u32);
+                } else {
+                    c.push(c[(i-1) as usize] * (l - i + 1)/i);
+                }
+            }
+            v.push(c);
         }
+        v
     }
 }
