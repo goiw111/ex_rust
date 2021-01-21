d6a70d2d8ad936a20aaf95cb3e2c8939773f14a5
sohayl chahmot <chahmot.s@gmail.com>
2021-01-21 19:51:35 +0100

    start-sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,5 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    (2..=upper_bound)
+        .filter(|&x| (x % 2 != 0 || x == 2) && (x % 3 != 0 || x == 3) && (x % 5 != 0 || x == 5) && (x % 7 != 0 || x == 7) && (x % 11 != 0 || x == 11))
+        .collect()
 }
